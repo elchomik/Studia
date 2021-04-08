9f73beb73c40f5f97ed4ffcfd6a849b941a602a2
Mateusz M <mateuszmarkowski101@wp.pl>
2021-04-08 20:36:39 +0200

    Add algorithm lista jednokierunkowa

---
+++ AISD/lista_jednokierunkowa/main.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct el_lista {
+    struct el_lista *nastepny;
+    int x;
+};
+struct lista{
+    struct el_lista *poczatek;
+    struct el_lista *koniec;
+};
+struct lista *dodaj(struct lista *l, int el){ // dodanie elementu
+    struct el_lista * new_el_lista =(struct el_lista*)malloc(sizeof(struct el_lista));
+    new_el_lista->x=el;
+    new_el_lista->nastepny=NULL;
+    struct el_lista *tmp;
+    if (l->koniec==NULL){
+         l->poczatek=new_el_lista;
+         l->koniec=new_el_lista;
+    }
+    else {
+      l->koniec->nastepny=new_el_lista;
+      l->koniec=new_el_lista;
+    }
+}
+struct lista* wydruk(struct lista *l){
+   if(l->poczatek==NULL){
+      printf("Lista pusta\n");
+      return 0;
+   }
+   else{
+      int i=1;
+      struct el_lista *tmp1;
+      tmp1=l->poczatek;
+      while(tmp1!=NULL){
+         printf("%d elementem listy jest: %d\n",i,tmp1->x);
+         i++;
+         tmp1=tmp1->nastepny;
+      }
+      return 1;
+   }
+}
+   int oblicz(struct lista *l){
+int ile=0;
+if(l->poczatek==NULL){
+    printf(" Brak elementow\n");
+    return 0;
+}
+else{
+    struct el_lista *tymczasowy;
+    tymczasowy=l->poczatek;
+    while(tymczasowy){
+        if(tymczasowy->x<0){
+            ile++;
+        }
+        tymczasowy=tymczasowy->nastepny;
+    }
+    return ile;
+}
+}
+
+int main()
+{
+   struct lista *l;
+   l = (struct lista*)malloc(sizeof(struct lista));
+   l->poczatek=NULL;
+   l->koniec=NULL;
+   int elem,miejsce,wybor,rob1,ile;
+   do {
+        system("cls");
+        printf("1. Dodaj element na koniec listy\n");
+        printf("2. Wyswietl elementy listy\n");
+        printf("3. Oblicz ile jest parzystych elementow listy\n");
+        printf("4. Zakoncz program\n");
+        printf("Twoj wybor : \n");
+        scanf("%d", &wybor);
+        switch (wybor){
+            case 1 : { // dodaj element do listy w wybranym miejscu
+                printf("Podaj wartosc do dodania : \n");
+                scanf("%d", &elem);
+                dodaj(l,elem);
+                break;
+            }
+            case 2 : { // pokaz elementy listy
+               wydruk(l);
+               system("PAUSE");
+               getchar();
+               break;
+            }
+            case 3 : {
+               ile = oblicz(l);
+               printf("Znaleziono %d ujemnych elementow \n", ile);
+               system("PAUSE");
+               getchar();
+               break;
+            }
+        }
+   } while (wybor != 4);
+   return 0;
+}
