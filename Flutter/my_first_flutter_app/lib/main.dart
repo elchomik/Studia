f27125d2e409e050a768be6b5e90cf4349bfabd8
Mateusz M <mateuszmarkowski101@wp.pl>
2021-05-02 22:50:05 +0200

    Add Flutter folder with first flutter app

---
+++ Flutter/my_first_flutter_app/lib/main.dart
@@ -0,0 +1,25 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello Flutter")
+        ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("Click"),
+      ),
+      ),
+    ),
+
+  );
+}
+
