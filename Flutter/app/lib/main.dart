e3379660434e4aeab2fb5dc078f424f80bd012e9
Mateusz M <mateuszmarkowski101@wp.pl>
2021-05-05 17:57:56 +0200

    Change basic concepts of Flutter app

--- Flutter/app/lib/main.dart
+++ Flutter/app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,9 +19,38 @@ class Home extends StatelessWidget{
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
-      body: Center(
-        child:Image.asset('assets/picture2.jpg'),
-
+      body:Row(
+        children: [
+          Expanded(
+              child: Image.asset("assets/picture.jpg"),
+            flex:3,
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30),
+              color: Colors.cyan,
+              child: Text("1"),
+            ),
+          ),
+          Expanded(
+            flex:4,
+            child: Container(
+              padding:EdgeInsets.all(30),
+              color: Colors.deepOrange,
+              child:Text("2"),
+            ),
+          ),
+          Expanded(
+            flex:3,
+            child:
+            Container(
+              padding: EdgeInsets.all(30),
+              color: Colors.amber,
+              child: Text("3"),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red[600],
