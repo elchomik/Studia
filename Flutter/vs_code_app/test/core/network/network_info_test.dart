23c4a3fc07e590edaa5386eae598e9ace756c847
elChomik <mateuszmarkowski101@wp.pl>
2022-02-21 21:56:35 +0100

    Fixed networkInfoImpl constructor parameter inside test file

--- Flutter/vs_code_app/test/core/network/network_info_test.dart
+++ Flutter/vs_code_app/test/core/network/network_info_test.dart
@@ -13,7 +13,7 @@ void main(){
 
   setUp((){
     mockDataConnectionChecker=MockDataConnectionChecker();
-    networkInfo=NetworkInfoImpl(mockDataConnectionChecker);
+    networkInfo=NetworkInfoImpl(connectionChecker: mockDataConnectionChecker);
   });
 
   group('isConnected',(){
