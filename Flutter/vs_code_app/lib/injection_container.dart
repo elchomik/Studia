3e74b1a8a11a08a5a2ecbcb49528f41f1024baf6
elChomik <mateuszmarkowski101@wp.pl>
2022-03-09 09:25:31 +0100

    Created dependency injection container

---
+++ Flutter/vs_code_app/lib/injection_container.dart
@@ -0,0 +1,46 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:vs_code_app/core/network/network_info.dart';
+import 'package:vs_code_app/core/util/input_converter.dart';
+import 'package:vs_code_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:vs_code_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:vs_code_app/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:vs_code_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:vs_code_app/features/number_trivia/domain/usecases/get_concete_number_trivia.dart';
+import 'package:vs_code_app/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:vs_code_app/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:http/http.dart' as http;
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features - Number Trivia
+
+  //Bloc
+  sl.registerFactory(() =>
+      NumberTriviaBloc(concrete: sl(), random: sl(), inputConverter: sl()));
+
+  //Use cases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+
+  //Repository
+  sl.registerLazySingleton<NumberTriviaRepository>(() =>
+      NumberTriviaRepositoryImpl(
+          remoteDataSource: sl(), localDataSource: sl(), networkInfo: sl()));
+
+  //Data sources
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(() => NumberTriviaRemoteDataSourceImpl(client: sl()));
+  sl.registerLazySingleton<NumberTriviaLocalDataSource>(() => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()));
+
+  //! Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(connectionChecker: sl()));
+
+
+  //! External
+  final sharedPreferences=await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
