01cbf5f3523533c87c04784b3f9e47a16677d5ef
elChomik <mateuszmarkowski101@wp.pl>
2022-03-16 17:43:46 +0100

    Finished pt3 of DDD flutter tutorial

--- Flutter/firebase_flutter_ddd/lib/domain/auth/email_address.dart
+++ Flutter/firebase_flutter_ddd/lib/domain/auth/email_address.dart
@@ -1,45 +1,26 @@
-import 'dart:html';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_flutter_ddd/domain/core/failures.dart';
+import 'package:firebase_flutter_ddd/domain/core/value_objects.dart';
 
-import 'package:flutter/widgets.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+import '../core/value_validators.dart';
 
-import 'package:meta/meta.dart';
-
-@immutable
-class EmailAddress {
-  final String value;
+class EmailAddress extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
 
   factory EmailAddress(String input) {
     return EmailAddress._(validateEmailAddress(input));
   }
 
   const EmailAddress._(this.value);
-
-  @override
-  String toString() => 'EmailAddress($value)';
-
-  @override
-  bool operator ==(Object o) {
-    if (identical(this, o)) return true;
-    return o is EmailAddress && o.value == value;
-  }
-
-  @override
-  int get hashCode => value.hashCode;
 }
 
-String validateEmailAddress(String input) {
-  const emailRegex =
-      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
-  if (RegExp(emailRegex).hasMatch(input)) {
-    return input;
-  } else {
-    throw InvalidEmailException(failedValue: input);
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(validatePassword(input));
   }
-}
 
-class InvalidEmailException implements Exception {
-  final String failedValue;
-
-  InvalidEmailException({required this.failedValue});
+  const Password._(this.value);
 }
