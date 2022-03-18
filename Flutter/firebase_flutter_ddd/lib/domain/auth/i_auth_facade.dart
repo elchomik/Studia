ebe7a7e354cb0eb6283973caca9c1f954872f92e
elChomik <mateuszmarkowski101@wp.pl>
2022-03-18 11:04:09 +0100

    Finished pt 4 of DDD with Firebase

---
+++ Flutter/firebase_flutter_ddd/lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_flutter_ddd/domain/auth/auth_failure.dart';
+import 'package:firebase_flutter_ddd/domain/auth/email_address.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
