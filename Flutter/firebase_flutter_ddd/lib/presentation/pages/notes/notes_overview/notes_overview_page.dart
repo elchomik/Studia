9d04843a596b7fbfd8edf5d40788749edb8fa822
elChomik <mateuszmarkowski101@wp.pl>
2022-05-15 22:34:25 +0200

    Finished pt22 of DDD course

--- Flutter/firebase_flutter_ddd/lib/presentation/pages/notes/notes_overview/notes_overview_page.dart
+++ Flutter/firebase_flutter_ddd/lib/presentation/pages/notes/notes_overview/notes_overview_page.dart
@@ -5,6 +5,7 @@ import 'package:firebase_flutter_ddd/application/notes/note_actor/bloc/note_acto
 import 'package:firebase_flutter_ddd/application/notes/note_watcher/bloc/note_watcher_bloc.dart';
 import 'package:firebase_flutter_ddd/injection.dart';
 import 'package:firebase_flutter_ddd/presentation/pages/notes/notes_overview/widgets/notes_overview_body_widget.dart';
+import 'package:firebase_flutter_ddd/presentation/pages/notes/notes_overview/widgets/uncompleted_switch.dart';
 import 'package:firebase_flutter_ddd/presentation/routes/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -59,9 +60,7 @@ class NotesOverviewPage extends StatelessWidget {
               icon: const Icon(Icons.exit_to_app),
             ),
             actions: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.indeterminate_check_box))
+              UncompletedSwitch(),
             ],
           ),
           body: NotesOverviewBody(),
