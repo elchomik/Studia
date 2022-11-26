403fd9c7490d84de63ab4b57ec0aee6b2262239e
elChomik <mateuszmarkowski101@wp.pl>
2022-11-26 22:12:57 +0100

    Create configuration for PAI app

---
+++ Java Spring/app/app/src/main/java/com/example/app/configuration/SecurityConfig.java
@@ -0,0 +1,92 @@
+package com.example.app.configuration;
+
+import com.example.app.services.AccountService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AccountService accountService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final String secret;
+
+    public SecurityConfig(final AccountService accountService,
+                          final AuthenticationConfiguration authenticationConfiguration,
+                          final @Value("${jwt.secret}") String secret) {
+        this.accountService = accountService;
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.secret = secret;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/register","/loginToApp").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter((new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration),
+                        secret,accountService)))
+                .exceptionHandling()
+                .defaultAuthenticationEntryPointFor(registerAccountEntryPoint(), new AntPathRequestMatcher("/register"));
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(accountService);
+        daoAuthenticationProvider.setPasswordEncoder(encoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsManager usersDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select username, password, email " +
+                "from accounts where username=?");
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2A);
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSucrityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**","/js/**","/webjars/**");
+    }
+
+    @Bean
+    public AuthenticationEntryPoint registerAccountEntryPoint(){
+        return new LoginUrlAuthenticationEntryPoint("/register");
+    }
+
+}
