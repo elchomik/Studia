971ecd5e254b6ae5d77371520802d6d76d9f42f8
elChomik <mateuszmarkowski101@wp.pl>
2022-11-26 22:24:33 +0100

    Reorganized files

--- Java Spring/app/app/src/main/java/com/example/app/repositories/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.repositories;
-
-import com.example.app.entities.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findAccountByUsername(final String login);
-
-}
