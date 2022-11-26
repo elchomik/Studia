af72af55f766d139a1fe476058b4694fc0d334a6
elChomik <mateuszmarkowski101@wp.pl>
2022-11-26 22:30:59 +0100

    Removed unused files

--- Java Spring/primeNumbers/primeNumbers/src/main/java/GameType.java
+++
@@ -1,5 +0,0 @@
-public enum GameType {
-    STRIKE,
-    SPARE,
-    NORMAL
-}
--- Java Spring/wallet/wallet/src/main/java/com/example/wallet/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.wallet.repositories;
-
-import com.example.wallet.readonly.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository  extends JpaRepository<User,Integer> {
-        User findUserByLogin(String login);
-}
