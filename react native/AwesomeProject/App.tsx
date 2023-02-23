03c0818f080c6803e156b534261e5e173bb9938b
elChomik <mateuszmarkowski101@wp.pl>
2023-02-23 19:43:17 +0100

    Modified App.tsx file

--- react native/AwesomeProject/App.tsx
+++ react native/AwesomeProject/App.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import {View, Text, Image, ScrollView, TextInput} from 'react-native';
 import Cat from "./Cat";
 import CatCaffe from "./CatCaffe";
+import FixedDimensionsBasic from "./FixedDimensions";
+import FlatListBasic from "./FlatListExample";
+import Flex from "./Flex";
+import FlexDirectionBasics from "./FlexDirectionBasics";
+import NetworkApp from "./Networking";
 import PizzaTranslator from "./Pizza";
+import ScrollViewExample from "./ScrollViewExample";
+import LotsOfStyle from "./StyleView";
 
 const App = () => {
-  return <ScrollView>
+  //return <NetworkApp/>;
+  // return <LotsOfStyle/>;
+  //return <FixedDimensionsBasic/>;
+  // return <Flex/>;
+  return <FlexDirectionBasics/>;
     {/* <Text> This is simple text which is displayed</Text>
     <View>
       <Text> Some more text</Text>
@@ -26,9 +37,9 @@ const App = () => {
     />
     <Cat name="Johny"/>
     <Cat name="Matt"/> */}
-    <CatCaffe/>
-    <PizzaTranslator/>
-  </ScrollView>
+    {/* <CatCaffe/>
+    <PizzaTranslator/> */}
+
 }
 
 export default App;
