bf2b14b9ff47a3ebca38a961c166e5d96caed226
elChomik <mateuszmarkowski101@wp.pl>
2023-03-30 22:31:18 +0200

    Created Processor class represented Processor from file

---
+++ Java Spring/T1/src/main/java/Processor.java
@@ -0,0 +1,37 @@
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+class Processor {
+
+    private String name;
+
+    @XmlElement(name = "physical_cores")
+    private String physicalCores;
+
+    private Processor() {
+    }
+
+    @XmlElement(name = "clock_speed")
+    private String clockSpeed;
+
+    public Processor(String name, String physicalCores, String clockSpeed) {
+        this.name = name;
+        this.physicalCores = physicalCores;
+        this.clockSpeed = clockSpeed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhysicalCores() {
+        return physicalCores;
+    }
+
+    public String getClockSpeed() {
+        return clockSpeed;
+    }
+
+}
