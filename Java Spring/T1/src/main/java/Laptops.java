f89b2543a4b2eea3cae4c05e29fba19fea81c738
elChomik <mateuszmarkowski101@wp.pl>
2023-03-30 22:32:19 +0200

    Created Laptops class represented Laptops from file

---
+++ Java Spring/T1/src/main/java/Laptops.java
@@ -0,0 +1,31 @@
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlRootElement(name = "laptops")
+@XmlAccessorType(XmlAccessType.FIELD)
+class Laptops {
+
+
+    @XmlElement(name = "laptop")
+    private List<Laptop> laptops;
+
+    @XmlAttribute(name = "moddate")
+    private String moddate;
+
+    public String getModdate() {
+        return moddate;
+    }
+
+    public void setModdate(String moddate) {
+        this.moddate = moddate;
+    }
+
+    public List<Laptop> getLaptops() {
+        return laptops;
+    }
+
+    public void setLaptops(List<Laptop> laptops) {
+        this.laptops = laptops;
+    }
+
+}
