2ac919f7883f136abbd645cd51321a2c535201d3
elChomik <mateuszmarkowski101@wp.pl>
2023-03-30 22:30:59 +0200

    Created Screen class represented Screen from file

---
+++ Java Spring/T1/src/main/java/Screen.java
@@ -0,0 +1,42 @@
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+class Screen {
+
+    private String size;
+
+    @XmlAttribute(name = "touch")
+    private String touch;
+
+    private String resolution;
+
+    private String type;
+
+    private Screen() {
+    }
+
+    public Screen(String size, String touch, String resolution, String type) {
+        this.size = size;
+        this.touch = touch;
+        this.resolution = resolution;
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+    public String getTouch() {
+        return touch;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
