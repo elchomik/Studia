6d58c72d7bee8448aa06cc32f114f8a0ef8da5c3
elChomik <mateuszmarkowski101@wp.pl>
2023-03-31 21:41:38 +0200

    Added parameterized test to validate if column should use String validation

--- Java Spring/T1/src/test/java/CatalogueTest.java
+++ Java Spring/T1/src/test/java/CatalogueTest.java
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -57,5 +60,19 @@ public class CatalogueTest {
 
         //assert
         assertFalse(catalogue.isInputContainsOnlyDigit("12323ssdds23"));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Column with String validation")
+    @ValueSource(ints ={1,4,5,6,11,12,14,15})
+    void isColumnWithStringValidation_InputIsInt_True(int column){
+        //given
+        Catalogue catalogue = mock(Catalogue.class);
+
+        //when
+        when(catalogue.isColumnWithStringValidation(column)).thenReturn(true);
+
+        //assertTrue
+        assertTrue(catalogue.isColumnWithStringValidation(column));
     }
 }
