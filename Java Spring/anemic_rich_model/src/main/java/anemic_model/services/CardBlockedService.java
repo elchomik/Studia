0efe8034ac60140ef39cb024f937887c6a6d30c0
elChomik <mateuszmarkowski101@wp.pl>
2023-04-15 15:32:42 +0200

    Added card blocked service :)

---
+++ Java Spring/anemic_rich_model/src/main/java/anemic_model/services/CardBlockedService.java
@@ -0,0 +1,35 @@
+package anemic_model.services;
+
+import anemic_model.BlockedCardException;
+import anemic_model.infrastructure.CardRepository;
+import anemic_model.model.Card;
+
+import java.util.UUID;
+
+public class CardBlockedService {
+
+    private final CardRepository cardRepository;
+
+    public CardBlockedService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    private void blockCard(UUID cardId) {
+        Card card = cardRepository.loadCardById(cardId).orElseThrow(IllegalArgumentException::new);
+
+        validateIsCardBlocked(card);
+        validateIsCardNotActive(card);
+
+        card.setBlocekd(true);
+
+        cardRepository.save(card);
+    }
+
+    private void validateIsCardNotActive(Card card) {
+        if(!card.isActive()) throw new BlockedCardException("Card couldn't be blocked");
+    }
+
+    private void validateIsCardBlocked(Card card) {
+        if(card.isBlocekd()) throw new BlockedCardException("Card couldn't be blocked");
+    }
+}
