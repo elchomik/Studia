367bfd644b25372bc91f7c7cf07c8ee622a91710
elChomik <mateuszmarkowski101@wp.pl>
2023-04-15 15:32:58 +0200

    Added card limit service :)

---
+++ Java Spring/anemic_rich_model/src/main/java/anemic_model/services/CardLimitService.java
@@ -0,0 +1,42 @@
+package anemic_model.services;
+
+
+import anemic_model.ChangedCardLimitException;
+import anemic_model.infrastructure.CardRepository;
+import anemic_model.model.Card;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+public class CardLimitService {
+
+    private final CardRepository cardRepository;
+
+    public CardLimitService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public void changeCardLimit(UUID cardId, BigDecimal limit) {
+        Card card = cardRepository.loadCardById(cardId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        validateIsCardBlocked(card);
+        validateIsCardNotActive(card);
+        validateCardLimit(limit);
+    }
+
+    private void validateCardLimit(BigDecimal limit) {
+        if(Objects.isNull(limit)) throw  new IllegalArgumentException("Limit was not provided");
+
+        if(limit.compareTo(BigDecimal.ONE) < 0) throw new ChangedCardLimitException("Limit is zero");
+    }
+
+    private void validateIsCardNotActive(Card card) {
+        if(!card.isActive()) throw new ChangedCardLimitException("Card is not active");
+    }
+
+    private void validateIsCardBlocked(Card card) {
+        if(card.isBlocekd()) throw new ChangedCardLimitException("Card is blocked");
+    }
+}
