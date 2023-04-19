033a833479e4f2dddc7525023c52499b5e8a074a
elChomik <mateuszmarkowski101@wp.pl>
2023-04-19 22:16:27 +0200

    Create connection DB class

--- Java Spring/T1/src/main/java/database/ConnectionDB.java
+++ Java Spring/T1/src/main/java/database/ConnectionDB.java
@@ -13,11 +13,16 @@ public class ConnectionDB {
         return statement.executeQuery("SELECT * FROM Urzadzenia");
     }
 
-    public static void extractDataToDB(Connection connection, DefaultTableModel tableModel, int rowCount) throws SQLException {
+    public static void deleteAllRecords(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("DELETE FROM Urzadzenia");
+    }
+
+    public static void extractDataToDB(Connection connection, DefaultTableModel tableModel) throws SQLException {
+
         for (int row = 0; row < tableModel.getRowCount(); row++) {
-            int id = ++rowCount;
-            String touch = tableModel.getValueAt(row, 1).toString();
-            String manufacturer = tableModel.getValueAt(row, 2).toString();
+            String manufacturer = tableModel.getValueAt(row, 1).toString();
+            String touch = tableModel.getValueAt(row, 2).toString();
             String screen_size = tableModel.getValueAt(row, 3).toString();
             String screen_resolution = tableModel.getValueAt(row, 4).toString();
             String screen_type = tableModel.getValueAt(row, 5).toString();
@@ -32,7 +37,7 @@ public class ConnectionDB {
             String os = tableModel.getValueAt(row, 14).toString();
             String disc_reader = tableModel.getValueAt(row, 15).toString();
 
-            insertToDB(connection, id, touch, manufacturer, screen_size, screen_resolution, screen_type,
+            insertToDB(connection,manufacturer, touch, screen_size, screen_resolution, screen_type,
                     processor_name, physical_cores, clock_speed,
                     ram, disc_type, disc_storage, graphic_card_name, graphic_card_memory, os, disc_reader);
         }
@@ -42,27 +47,26 @@ public class ConnectionDB {
         return input.equals("Brak") ? 0 : Integer.parseInt(input);
     }
 
-    private static void insertToDB(Connection connection, int id, String touch, String manufacturer, String screen_size, String screen_resolution, String screen_type, String processor_name, int physical_cores, int clock_speed, String ram, String disc_type, String disc_storage, String graphic_card_name, String graphic_card_memory, String os, String disc_reader) throws SQLException {
-        String sql = "INSERT INTO Urzadzenia (id, touch, manufacturer, screen_size, screen_resolution, screen_type, processor_name, physical_cores, clock_speed, ram, disc_type, disc_storage, graphic_card_name, graphic_card_memory, os, disc_reader) "
-                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static void insertToDB(Connection connection,String manufacturer, String touch, String screen_size, String screen_resolution, String screen_type, String processor_name, int physical_cores, int clock_speed, String ram, String disc_type, String disc_storage, String graphic_card_name, String graphic_card_memory, String os, String disc_reader) throws SQLException {
+        String sql = "INSERT INTO Urzadzenia (manufacturer, touch, screen_size, screen_resolution, screen_type, processor_name, physical_cores, clock_speed, ram, disc_type, disc_storage, graphic_card_name, graphic_card_memory, os, disc_reader) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setInt(1, id);
+        statement.setString(1, manufacturer);
         statement.setString(2, touch);
-        statement.setString(3, manufacturer);
-        statement.setString(4, screen_size);
-        statement.setString(5, screen_resolution);
-        statement.setString(6, screen_type);
-        statement.setString(7, processor_name);
-        statement.setInt(8, physical_cores);
-        statement.setInt(9, clock_speed);
-        statement.setString(10, ram);
-        statement.setString(11, disc_type);
-        statement.setString(12, disc_storage);
-        statement.setString(13, graphic_card_name);
-        statement.setString(14, graphic_card_memory);
-        statement.setString(15, os);
-        statement.setString(16, disc_reader);
+        statement.setString(3, screen_size);
+        statement.setString(4, screen_resolution);
+        statement.setString(5, screen_type);
+        statement.setString(6, processor_name);
+        statement.setInt(7, physical_cores);
+        statement.setInt(8, clock_speed);
+        statement.setString(9, ram);
+        statement.setString(10, disc_type);
+        statement.setString(11, disc_storage);
+        statement.setString(12, graphic_card_name);
+        statement.setString(13, graphic_card_memory);
+        statement.setString(14, os);
+        statement.setString(15, disc_reader);
 
         statement.executeUpdate();
     }
