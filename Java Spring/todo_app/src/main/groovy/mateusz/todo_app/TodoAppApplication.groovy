9e4f6ee6cbebe20705f6311bdaebd19c7c571304
elChomik <mateuszmarkowski101@wp.pl>
2023-04-30 17:16:26 +0200

    Finished first web app in groovy

---
+++ Java Spring/todo_app/src/main/groovy/mateusz/todo_app/TodoAppApplication.groovy
@@ -0,0 +1,13 @@
+package mateusz.todo_app
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TodoAppApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TodoAppApplication, args)
+    }
+
+}
