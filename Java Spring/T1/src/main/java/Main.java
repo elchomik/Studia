e71f76a6a70b8bf22ef4d32026be33d09c52dedc
elChomik <mateuszmarkowski101@wp.pl>
2023-05-14 15:22:54 +0200

    T5: Added client to main method

--- Java Spring/T1/src/main/java/Main.java
+++ Java Spring/T1/src/main/java/Main.java
@@ -9,10 +9,10 @@ import static javax.xml.ws.Endpoint.publish;
 
 public class Main {
 
-    public static void main(String[] args) throws MalformedURLException, SQLException {
+    public static void main(String[] args) {
         new Server();
         publish("http://localhost:8080/laptops",new LaptopSoapInterfaceImpl());
-        new Client("matowy");
+        new Client();
     }
 
 }
