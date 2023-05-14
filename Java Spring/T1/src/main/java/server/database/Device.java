e408064be1f5ac59e7ee559e1890ca181e20dbb5
elChomik <mateuszmarkowski101@wp.pl>
2023-05-14 15:19:24 +0200

    T5: Added annotation that allows to handle XML exchange by client and server

--- Java Spring/T1/src/main/java/server/database/Device.java
+++ Java Spring/T1/src/main/java/server/database/Device.java
@@ -1,8 +1,13 @@
 package server.database;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 import java.util.Objects;
 
+@XmlRootElement(name = "device")
+@XmlAccessorType(XmlAccessType.FIELD)
 public final class Device {
     private int id;
     private String manufacturer;
