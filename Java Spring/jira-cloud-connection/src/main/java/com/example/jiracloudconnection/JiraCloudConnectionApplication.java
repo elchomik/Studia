440c8c3e9d90bc0abf9d60dffefeafa8ff51eefc
elChomik <mateuszmarkowski101@wp.pl>
2023-06-17 23:06:07 +0200

    Create main app in sprign boot

---
+++ Java Spring/jira-cloud-connection/src/main/java/com/example/jiracloudconnection/JiraCloudConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.jiracloudconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraCloudConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraCloudConnectionApplication.class, args);
+    }
+
+}
