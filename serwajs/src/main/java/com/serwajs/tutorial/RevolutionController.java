0e965443d09f1588ef2a5b4cab26278bd46d744b
elChomik <mateuszmarkowski101@wp.pl>
2024-01-19 22:56:23 +0100

    Modified Revolution Controller

--- serwajs/src/main/java/com/serwajs/tutorial/RevolutionController.java
+++ serwajs/src/main/java/com/serwajs/tutorial/RevolutionController.java
@@ -8,17 +8,32 @@ class RevolutionController {
 
     private final CamundaFacade camundaFacade;
 
-    RevolutionController(CamundaFacade camundaFacade) {
+    RevolutionController(final CamundaFacade camundaFacade) {
         this.camundaFacade = camundaFacade;
     }
 
     @PostMapping("/start/{key}")
-    public String startRevolution(@PathVariable String key) {
+    public String startRevolution(@PathVariable final String key) {
         return camundaFacade.startProcessById(key);
     }
 
     @GetMapping("/isRunning/{processInstanceId}")
-    public Boolean isProcessRunning(@PathVariable String processInstanceId) {
+    public Boolean isProcessRunning(@PathVariable final String processInstanceId) {
         return camundaFacade.isProcessInstanceIsRunning(processInstanceId);
+    }
+
+    @PostMapping("/start/overthrowMonarchy")
+    public String startProcessFromMonarchy(@RequestParam final String key) {
+        return camundaFacade.startProcessBeforeOverthrowMonarchy(key);
+    }
+
+    @PostMapping("/start/taxi")
+    public String startProcessFromTaxi(@RequestParam final String key) {
+        return camundaFacade.startProcessBeforeTaxi(key);
+    }
+
+    @PostMapping("/start/departure")
+    public String startProcessFromDeparture(@RequestParam final String key) {
+        return camundaFacade.startProcessBeforeDeparture(key);
     }
 }
